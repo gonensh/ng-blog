c0dcc3ddd26c0bc8724a7398a59d2876349fefa2
Gonen Shoham <Gonen_Shoham@vfc.com>
2019-02-16 18:45:10 -0800

    Move Post model into the models folder

--- src/app/posts/post-tile/post-tile.component.ts
+++ src/app/posts/post-tile/post-tile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Post } from '../post';
+import { Post } from '../../models/post.model';
 
 @Component({
   selector: 'app-post-tile',
