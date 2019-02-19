0a7daf702ae1b0907abde8bc6125a845dde80c9c
Gonen Shoham <Gonen_Shoham@vfc.com>
2019-02-19 01:06:22 -0800

    Specify Post image and categories as optional

--- src/app/models/post.model.ts
+++ src/app/models/post.model.ts
@@ -3,6 +3,15 @@ export class Post {
   userId: number;
   title: string;
   body: string;
-  image: string;
-  categories: string[];
+  image?: string;
+  categories?: string[];
+
+  constructor(post: Post) {
+    this.id = post.id;
+    this.userId = post.userId;
+    this.title = post.title;
+    this.body = post.body;
+    this.image = post.image || '';
+    this.categories = post.categories || [];
+  }
 }
