16d6dc8752f208ec568b95ada64ebb2f136367ba
Gonen Shoham <Gonen_Shoham@vfc.com>
2019-02-18 23:05:22 -0800

    Add style and UI functionality to the filters component

--- src/app/posts/filters/filters.component.ts
+++ src/app/posts/filters/filters.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
   styleUrls: ['./filters.component.scss']
 })
-export class FiltersComponent implements OnInit {
-  categories: string[] = ['all', 'web', 'dev', 'design'];
+export class FiltersComponent {
+  categories: string[] = ['All', 'Digital', 'Web', 'Print'];
+  selectedCategory: string = this.categories[0];
+
   constructor() {}
 
-  ngOnInit() {}
+  private selectCategory(category: string) {
+    this.selectedCategory = category;
+  }
 }
