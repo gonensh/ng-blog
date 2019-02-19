bdb333e50778821f090c07f9fbd29ec44fd92f82
Gonen Shoham <Gonen_Shoham@vfc.com>
2019-02-19 02:16:20 -0800

    Add unit tests for the user and post services

--- src/app/services/post.service.spec.ts
+++ src/app/services/post.service.spec.ts
@@ -1,5 +1,8 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 
 import { PostService } from './post.service';
 
@@ -14,4 +17,50 @@ describe('PostService', () => {
     const service: PostService = TestBed.get(PostService);
     expect(service).toBeTruthy();
   });
+
+  it('should fetch post data from the Typicode API endpoint', inject(
+    [HttpTestingController, PostService],
+    (httpMock: HttpTestingController, postService: PostService) => {
+      postService.getPosts(2).subscribe(data => {
+        expect(data.length).toBe(4);
+        expect(data[2].id).toEqual(13);
+      });
+
+      const req = httpMock.expectOne(
+        'https://jsonplaceholder.typicode.com/posts?userId=2'
+      );
+      expect(req.request.method).toEqual('GET');
+
+      req.flush([
+        {
+          userId: 2,
+          id: 11,
+          title: 'et ea vero quia laudantium autem',
+          body:
+            'delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus'
+        },
+        {
+          userId: 2,
+          id: 12,
+          title: 'in quibusdam tempore odit est dolorem',
+          body:
+            'itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil'
+        },
+        {
+          userId: 2,
+          id: 13,
+          title: 'dolorum ut in voluptas mollitia et saepe quo animi',
+          body:
+            'aut dicta possimus sint mollitia voluptas commodi quo doloremque\niste corrupti'
+        },
+        {
+          userId: 2,
+          id: 14,
+          title: 'voluptatem eligendi optio',
+          body:
+            'fuga et accusamus dolorum perferendis illo voluptas\nnon doloremque neque facere\nad qui '
+        }
+      ]);
+    }
+  ));
 });
--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,5 +1,8 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 
 import { UserService } from './user.service';
 
@@ -14,4 +17,114 @@ describe('UserService', () => {
     const service: UserService = TestBed.get(UserService);
     expect(service).toBeTruthy();
   });
+
+  it('should fetch user data from the Typicode API endpoint', inject(
+    [HttpTestingController, UserService],
+    (httpMock: HttpTestingController, userService: UserService) => {
+      userService.getUsers().subscribe(data => {
+        expect(data.length).toBe(4);
+        expect(data[2].username).toEqual('Samantha');
+      });
+
+      const req = httpMock.expectOne(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      expect(req.request.method).toEqual('GET');
+
+      req.flush([
+        {
+          id: 1,
+          name: 'Leanne Graham',
+          username: 'Bret',
+          email: 'Sincere@april.biz',
+          address: {
+            street: 'Kulas Light',
+            suite: 'Apt. 556',
+            city: 'Gwenborough',
+            zipcode: '92998-3874',
+            geo: {
+              lat: '-37.3159',
+              lng: '81.1496'
+            }
+          },
+          phone: '1-770-736-8031 x56442',
+          website: 'hildegard.org',
+          company: {
+            name: 'Romaguera-Crona',
+            catchPhrase: 'Multi-layered client-server neural-net',
+            bs: 'harness real-time e-markets'
+          }
+        },
+        {
+          id: 2,
+          name: 'Ervin Howell',
+          username: 'Antonette',
+          email: 'Shanna@melissa.tv',
+          address: {
+            street: 'Victor Plains',
+            suite: 'Suite 879',
+            city: 'Wisokyburgh',
+            zipcode: '90566-7771',
+            geo: {
+              lat: '-43.9509',
+              lng: '-34.4618'
+            }
+          },
+          phone: '010-692-6593 x09125',
+          website: 'anastasia.net',
+          company: {
+            name: 'Deckow-Crist',
+            catchPhrase: 'Proactive didactic contingency',
+            bs: 'synergize scalable supply-chains'
+          }
+        },
+        {
+          id: 3,
+          name: 'Clementine Bauch',
+          username: 'Samantha',
+          email: 'Nathan@yesenia.net',
+          address: {
+            street: 'Douglas Extension',
+            suite: 'Suite 847',
+            city: 'McKenziehaven',
+            zipcode: '59590-4157',
+            geo: {
+              lat: '-68.6102',
+              lng: '-47.0653'
+            }
+          },
+          phone: '1-463-123-4447',
+          website: 'ramiro.info',
+          company: {
+            name: 'Romaguera-Jacobson',
+            catchPhrase: 'Face to face bifurcated interface',
+            bs: 'e-enable strategic applications'
+          }
+        },
+        {
+          id: 4,
+          name: 'Patricia Lebsack',
+          username: 'Karianne',
+          email: 'Julianne.OConner@kory.org',
+          address: {
+            street: 'Hoeger Mall',
+            suite: 'Apt. 692',
+            city: 'South Elvis',
+            zipcode: '53919-4257',
+            geo: {
+              lat: '29.4572',
+              lng: '-164.2990'
+            }
+          },
+          phone: '493-170-9623 x156',
+          website: 'kale.biz',
+          company: {
+            name: 'Robel-Corkery',
+            catchPhrase: 'Multi-tiered zero tolerance productivity',
+            bs: 'transition cutting-edge web services'
+          }
+        }
+      ]);
+    }
+  ));
 });
